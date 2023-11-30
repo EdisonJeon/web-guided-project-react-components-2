15327a45961daa835472eeb103899239985fb64a
Edison Jeon <edisonthe2nd@gmail.com>
2023-11-30 12:39:57 -0800

    working through mvp

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,42 @@
-// This is the top-level component
-// so we'll keep application state at this level.
-// 👉 1- Import the state hook!
-import React from 'react'
-import FriendsList from './FriendsList'
-import Search from './Search'
-// 👉 2- Import the dummy data that will power the application.
-// (Tomorrow we'll fetch the data from an API instead.)
-
+import React, { useState } from "react";
+import FriendsList from "./FriendsList";
+import Search from "./Search";
+import dummyData from "../dummy-data/friends";
+import Friend from "./Friend";
 
 export default function App() {
-  // 👉 3- Initialize a slice of state to keep track of the data
-  // using the dummy data as the initial value of the slice of state
+  const [data, setData] = useState(dummyData);
+  const [search, setSearch] = useState("");
 
-  // 👉 4- Initialize a slice to keep track of the value of the search box
-  // using an empty string as the initial value of the slice
+  const changeStatus = (id) => {
+    setData(
+      data.map((elem) => {
+        if (elem.id === id) {
+          return { ...elem, married: !elem.married };
+        } else {
+          return elem;
+        }
+      })
+    );
+  };
 
-  // 👉 5- Build a `changeStatus` function that takes an id and
-  // changes the `married` from true to false and viceversa
-
-  // STRETCH - Make a helper function that returns
-  // a filtered array of friends data (filtering by search term)
+  const filteredArr = () => {
+    const term = search.trim().toLowerCase();
+    if (!term) return data;
+    return data.filter((elem) => {
+      return elem.name.toLowerCase().includes(term);
+    });
+  };
 
   return (
-    <div className='app-friends container'>
+    <div className="app-friends container">
       {/* 👉 6- Render the Search component */}
+      <Search setSearch={setSearch} />
+      <FriendsList filteredArr={filteredArr()} changeStatus={changeStatus} />
       {/* STRETCH - Changes to the input should update the search term */}
 
       {/* 👉 7- Render the FriendsList component */}
       {/* What prop/props does FriendsList need? */}
     </div>
-  )
+  );
 }
